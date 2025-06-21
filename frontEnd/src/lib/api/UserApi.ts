d88ad9b80c31ee0c698cd3373cbcaaddbdd2fc3a
Baskoro Ramadhan <baskoror725@gmail.com>
2025-06-21 14:28:25 +0700

    add api to update profile and password

--- frontEnd/src/lib/api/UserApi.ts
+++ frontEnd/src/lib/api/UserApi.ts
@@ -24,7 +24,7 @@ export const userRegister = async ({ username, password, name }: RegisterPayload
   });
 };
 
-export const userLogin = async ({ username, password}: LoginPayload) => {
+export const userLogin = async ({ username, password }: LoginPayload) => {
   return await fetch(`${import.meta.env.VITE_API_PATH}/users/login`, {
     method: 'POST',
     headers: {
@@ -33,7 +33,51 @@ export const userLogin = async ({ username, password}: LoginPayload) => {
     },
     body: JSON.stringify({
       username,
-      password, 
+      password,
     }),
+  });
+};
+
+export const userUpdateProfile = async (
+  token: string,
+  { name }: { name: string },
+): Promise<Response> => {
+  return await fetch(`${import.meta.env.VITE_API_PATH}/users/current`, {
+    method: 'PATCH',
+    headers: {
+      'Content-Type': 'application/json',
+      Accept: 'application/json',
+      Authorization: token,
+    },
+    body: JSON.stringify({
+      name,
+    }),
+  });
+};
+
+export const userUpdatePassword = async (
+  token: string,
+  { password }: { password: string },
+): Promise<Response> => {
+  return await fetch(`${import.meta.env.VITE_API_PATH}/users/current`, {
+    method: 'PATCH',
+    headers: {
+      'Content-Type': 'application/json',
+      Accept: 'application/json',
+      Authorization: token,
+    },
+    body: JSON.stringify({
+      password,
+    }),
+  });
+};
+
+export const userDetail = async (token: string): Promise<Response> => {
+  return await fetch(`${import.meta.env.VITE_API_PATH}/users/current`, {
+    method: 'GET',
+    headers: {
+      Accept: 'application/json',
+      Authorization: token,
+    },
   });
 };
